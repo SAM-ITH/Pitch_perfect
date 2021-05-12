5651f0c8de9d9e08a1eaee973620ba1103f860be
samith <samithwijesinha@gmail.com>
2021-05-12 21:19:54 +0530

    adding the images to the record button and size changes

--- Pitch_perfect/ViewController.swift
+++ Pitch_perfect/ViewController.swift
@@ -13,11 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("hey this thing is appears")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    
+    
     @IBAction func recordAudio(_ sender: Any) {
         print("working my nigga!!")
         recordLabel.text = "recording in progress"
     }
-    @IBOutlet weak var recordLabel: UILabel!
+ 
     @IBAction func stopRecording(_ sender: Any) {
        print("hmmm see if this works")
     }
